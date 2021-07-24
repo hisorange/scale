82a6dc3185a177fce4c9615875499f4048abae1a
Varga Zsolt <hello@hisorange.me>
2021-07-24 08:03:10 +0200

    Release 0.1.6

--- src/tests/test_vpn.py
+++ src/tests/test_vpn.py
@@ -3,6 +3,15 @@ from logging import Logger
 from scale.vpn import VPNManager
 
 
+class MockConfig:
+    def __init__(self, config):
+        self.__dict__.update(config)
+
+
 # should create the logger
 def test_should_create_logger():
-    assert isinstance(VPNManager({}).logger, Logger)
+    assert isinstance(VPNManager(MockConfig({
+        "network": {
+            "interface": "wga"
+        }
+    })).logger, Logger)

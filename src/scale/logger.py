953b16c85df9f18317d664220361ae6322878c65
Varga Zsolt <hello@hisorange.me>
2021-07-24 07:49:16 +0200

    Better visibility with colored loggers

--- src/scale/logger.py
+++ src/scale/logger.py
@@ -1,17 +1,16 @@
 import logging
 
+import coloredlogs
+
+fmt = '[%(asctime)s][%(levelname)8s] %(name)20s - %(message)s'
+coloredlogs.install(level=logging.DEBUG, fmt=fmt)
+
 
 def create_logger(name, level=logging.DEBUG, handler=None) -> logging.Logger:
     """
     Create a logger with the given name.
     """
     logger = logging.getLogger(name)
-    logger.setLevel(level)
     if handler is None:
         handler = logging.StreamHandler()
-    handler.setLevel(level)
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
     return logger
